86972b7f1d2d8457bd722a3c3c50f409d48e98d2
morris <ma.stranger01@gmail.com>
2021-01-16 21:03:25 +0000

    added working neighbor_bomb_sum method

--- Board.rb
+++ Board.rb
@@ -1,10 +1,46 @@
+require_relative "Tile.rb"
 class Board
-    def initialize(grid)
-        @grid=grid
+    # attr_accessor :grid
+    def initialize()
+        # create_board
+        # @grid=grid
+        
     end
-    def self.create_board 
-        Array.new(9) do 
-            Array.new(9) {Tile.new(bomb=false,board=self)}
+    
+    def create_board
+        # debu#gger
+        @grid=Array.new(9) do 
+            Array.new(9) {Tile.new(self)}
         end
+        indices=(0..8).to_a
+        already_seen=[]
+        50.times do 
+        i=indices.sample
+        j=indices.sample
+        
+        if !already_seen.include?([i,j])
+            @grid[i][j].makebomb
+        end
+            already_seen << [i,j]
+        end
+        @grid
+
     end
+    # def grid
+    # @grid
+    # end
+    def [](pos)
+    x,y=pos
+    @grid[x][y]
+    end
+def grid
+    @grid
 end
+end
+# p newboard
+
+# p newboard[[0,1]].neighbor_bomb_count
+# p newboard.grid
+newboard=Board.new()
+newboard.create_board
+p newboard[[0,1]].neighbor_bomb_count
--- Tile.rb
+++ Tile.rb
@@ -1,16 +1,60 @@
+require "byebug"
+# require_relative "Board.rb"
+# require_relative "Tile.rb"
 class Tile
-def initialize(revealed=false,bomb,flagged,board)
+    attr_reader :bomb
+    @@neighbors=[
+        [-1,-1],
+        [-1,0],
+        [-1,1],
+    [0,-1],
+    [0,1],
+        [1,-1],
+        [1,0],
+        [1,1]
+    ]
+  
+def initialize(revealed=false,bomb=false,flagged=false,board)
 @revealed = revealed
 @bomb=bomb
 @flagged=flagged
 @board=board
+
 end
 def reveal
 @revealed=true
 end
+def makebomb
+@bomb=true
+end
 def neighbors
 end
-def neighbor_bomb_count
+def sum_arrays(arr1,arr2)
+    nums = [arr1, arr2]
+
+    nums.transpose.map(&:sum) 
+end
+def bomb?(value)
+value ? 1 :0
+end
+def neighbor_bomb_count()
+# x,y=pos
+# debugger
+bomb_sum=0
+@board.grid.each.with_index do |row,i|
+
+    row.each.with_index do |tile,j|
+        if @board[[i,j]]==self
+        #    return i,j
+        @@neighbors.each do |neighbor|
+        index = sum_arrays([i,j],neighbor)
+        bomb_sum += bomb?(@board[index].bomb)
+        end
+        return bomb_sum
+        end
+    end
+    
+end
 
 end
 
